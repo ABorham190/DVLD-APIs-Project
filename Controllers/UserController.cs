a457aada68c0669a62ba1292091a26588b521a67
Ahmad Borham <ahmad.borham.dev@gmail.com>
2024-09-14 20:18:37 +0300

    adding User Controller

---
+++ Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dvld_api.Controllers
+{
+    [Route("api/User")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+    }
+}
