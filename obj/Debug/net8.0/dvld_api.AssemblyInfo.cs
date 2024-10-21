69136f6d6d17bb5328e21e1fab7e40181c48e0b0
Ahmad Borham <ahmad.borham.dev@gmail.com>
2024-10-21 20:38:45 +0300

    some changes

--- obj/Debug/net8.0/dvld_api.AssemblyInfo.cs
+++ obj/Debug/net8.0/dvld_api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dvld_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f84dbdc0350df27ff265665894ac21599741aac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+debb92a501e7902bff151ffaf9bd6fd18747e474")]
 [assembly: System.Reflection.AssemblyProductAttribute("dvld_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dvld_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
